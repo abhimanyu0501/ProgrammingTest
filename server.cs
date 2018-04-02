88f54f18a05422c4f95bf52b7c0d5e75a028fb3f
abhi <abhi@gmail.com>
2018-04-02 23:09:46 +0530

    test

---
+++ server.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MulServer
+{
+    class Program1
+    {
+        public void status()
+        {
+            Boolean temp = false;
+            IList list;
+            for(int i=1;i<=100;i++)
+            {
+
+                for(int j=i;j<=100;j=j*i)
+                {
+
+                }
+            }
+        }
+    }
+}
